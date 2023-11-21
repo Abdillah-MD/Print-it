2e0a60d0461884afabac845d206836353a5bd0d8
Abdillah-MD <ambdi6@gmail.com>
2023-11-21 14:46:47 +0100

    MAJ dots navigation

--- assets/script.js
+++ assets/script.js
@@ -1,76 +1,84 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
 ]
-let i = 0 
-console.log(slides[i])
 
+let i = 0
 
-// Récupérer les élements qui se trouvent dans le banner 
+// Récupérer les éléments qui se trouvent dans le banner
 let bannerImg = document.querySelector("#banner .banner-img")
-console.log(bannerImg)
-let dots = document.querySelector(".dots")
+let dotsContainer = document.querySelector(".dots")
 let bannerTxt = document.querySelector("#banner p")
 
 const affichage = (index) => {
-        bannerImg.src = './assets/images/slideshow/' + slides[index].image
-        bannerTxt.innerHTML = `<p>${slides[index].tagLine}</p>`
+    bannerImg.src = './assets/images/slideshow/' + slides[index].image
+    bannerTxt.innerHTML = `<p>${slides[index].tagLine}</p>`
 }
 
 affichage(i)
 
-/////////////////////////// Mes fonctions /////////////////////
 // Repérer la flèche gauche du slider
 let flecheGauche = document.querySelector("#banner .arrow_left")
-console.log(flecheGauche)
 flecheGauche.addEventListener("click", () => {
-	i-- 
-	if (i === -1) {i = slides.length - 1}
-	affichage(i)
-
-	console.log("Vous avez cliqué sur la flèche gauche." + " i = " + i)
+    i--
+    if (i === -1) {
+        i = slides.length - 1
+    }
+    affichage(i)
+    updateDots()
 })
 
 // Repérer la flèche droite du slider
 let flecheDroit = document.querySelector("#banner .arrow_right")
-console.log(flecheDroit)
-console.log(i)
 flecheDroit.addEventListener("click", () => {
-	i++
-	if (i === slides.length) {i = 0}
-	affichage(i)
-
-	console.log("Vous avez cliqué sur la flèche droite." + " i = " + i)
+    i++
+    if (i === slides.length) {
+        i = 0
+    }
+    affichage(i)
+    updateDots()
 })
 
-// Ecouter & Intéragir avec les dots 
-let dot = document.createElement("div")
-
-for (let nb = 0; nb < slides.length; nb++ ) {
-	dot = document.createElement("div")
-	dot.className = "dot"
-	dots.appendChild(dot)
-	console.log(dot)
+// Initialiser les dots
+for (let index = 0; index < slides.length; index++) {
+    let dot = document.createElement("div")
+    dot.className = "dot"
+    dotsContainer.appendChild(dot)
 }
 
 let nbDot = document.querySelectorAll(".dot")
-console.log(nbDot)
+nbDot[i].classList.add("dot_selected")
 
-nbDot.addEventListener("click", () => {
-	nbDot[i] = slides[i]
-	console.log("vous avez cliqué sur " + nbDot)
+// Mettre à jour les dots
+function updateDots() {
+    nbDot.forEach((dot, index) => {
+        if (index === i) {
+            dot.classList.add("dot_selected")
+        } else {
+            dot.classList.remove("dot_selected")
+        }
+    })
+}
+
+// Écouter & interagir avec les dots
+nbDot.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        i = index
+        affichage(i)
+        updateDots()
+    })
 })
